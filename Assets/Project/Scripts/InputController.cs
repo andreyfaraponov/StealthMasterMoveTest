417c64267f654a23a18b0480b4d1ab49155db586
Andrey Faraponov <andrey.faraponov@gmail.com>
2020-09-24 23:53:59 +0300

    Done

---
+++ Assets/Project/Scripts/InputController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputController : MonoBehaviour
+{
+	[SerializeField] private RectTransform stickBaseTransform;
+	[SerializeField] private RectTransform stickTransform;
+	[SerializeField] private RectTransform initialTransform;
+
+	private float stickBaseRadius;
+	private Vector2 startPos;
+
+	public event Action<Vector2> OnMoveStickEvent;
+
+	private void OnEnable() {
+		stickBaseRadius = stickBaseTransform.rect.width * .5f;
+	}
+
+	public void OnFingerDown(BaseEventData ev) {
+		startPos = Input.mousePosition;
+		stickBaseTransform.position = startPos;
+	}
+
+	public void OnFingerDrag(BaseEventData ev) {
+		StickPosition(Input.mousePosition);
+	}
+
+	public void OnFingerUp(BaseEventData ev) {
+		stickBaseTransform.position = initialTransform.position;
+		stickTransform.localPosition = Vector2.zero;
+		OnMoveStickEvent?.Invoke(Vector2.zero);
+	}
+
+	private void StickPosition(Vector2 pos) {
+
+		Vector2 centerPosition = stickBaseTransform.position;
+		float distance = Vector2.Distance(pos, centerPosition);
+
+		if (distance > stickBaseRadius) {
+			var direction = (pos - centerPosition);
+			direction *= stickBaseRadius / distance;
+			pos = centerPosition + direction;
+		}
+
+		stickTransform.position = pos;
+		var moveVector = (pos - startPos) / stickBaseRadius;
+		OnMoveStickEvent?.Invoke(moveVector);
+	}
+}
---
+++ Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] private InputController inputController;
+	[SerializeField] private Rigidbody playerRigidbody;
+	[SerializeField] private Animator playerAnimator;
+
+	[Range(1, 50)]
+	[SerializeField] private float playerMoveSpeed = 5;
+
+	private void OnEnable() {
+		inputController.OnMoveStickEvent += MovePlayer;
+		inputController.OnMoveStickEvent += UpdatePlayerView;
+	}
+
+	private void OnDisable() {
+		inputController.OnMoveStickEvent -= MovePlayer;
+		inputController.OnMoveStickEvent -= UpdatePlayerView;
+	}
+	Vector3 velocityVector;
+	private void MovePlayer(Vector2 moveVector) {
+		velocityVector.x = moveVector.x;
+		velocityVector.z = moveVector.y;
+		velocityVector.y = 0;
+		playerRigidbody.velocity = velocityVector * playerMoveSpeed;
+		if (moveVector.magnitude > 0.01f) { 
+			playerAnimator.transform.forward = velocityVector;
+		}
+	}
+
+	private void UpdatePlayerView(Vector2 direction) {
+		playerAnimator.SetFloat("moveBlend", direction.magnitude);
+	}
+}
